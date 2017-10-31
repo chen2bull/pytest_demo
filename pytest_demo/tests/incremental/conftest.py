ebe3fc8b1ec2d6753b20ee3804ccf3db544b9196
chenmingjian <cmj@ejoy.com>
2017-10-31 19:14:11 +0800

    测试|incremental

---
+++ pytest_demo/tests/incremental/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+# 某些测试case要求测试步骤, 如果前面的case错掉了,后面的全部为fail
+# 见以下两个方法
+def pytest_runtest_makereport(item, call):
+    if "incremental" in item.keywords:
+        if call.excinfo is not None:
+            parent = item.parent
+            parent._previousfailed = item
+
+
+def pytest_runtest_setup(item):
+    if "incremental" in item.keywords:
+        previousfailed = getattr(item.parent, "_previousfailed", None)
+        if previousfailed is not None:
+            pytest.xfail("previous test failed (%s)" % previousfailed.name)
